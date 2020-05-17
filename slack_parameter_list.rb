b95b3a5be7ecb3e7639d620c85555f1f19718289
himrock922 <himrock922@gmail.com>
2020-05-17 23:47:02 +0900

    Tokenマスキング

--- slack_parameter_list.rb
+++ slack_parameter_list.rb
@@ -3,7 +3,7 @@
 # Slack API経由で情報を取得するためんのAPIトークンやURIパラメータ等をまとめたリスト
 module SlackParameterList
   SLACK_URI = 'https://slack.com/api/'
-  API_TOKEN = 'xoxb-1121626440486-1128804071026-5bhWNfex1VJbOHeTvW6ziuii'
+  API_TOKEN = ''
   CONVERSATIONS_LIST = 'conversations.list?'
 end
 
